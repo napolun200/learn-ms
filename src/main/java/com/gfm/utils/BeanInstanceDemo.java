b53b2132296f1600ce81188f933344202cb62eb0
jack <napolun200@163.com>
2019-12-20 18:49:41 +0800

    ioc instance

--- src/main/java/com/gfm/utils/BeanInstanceDemo.java
+++ src/main/java/com/gfm/utils/BeanInstanceDemo.java
@@ -1,20 +1,34 @@
 package com.gfm.utils;
 
+import com.gfm.service.UserService;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.config.NamedBeanHolder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.NullBean;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.ResolvableType;
+import org.springframework.core.log.LogMessage;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BeanInstanceDemo {
 
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:application.xml");
+        UserService userService = context.getBean(UserService.class);
     }
 
     //AbstractApplicationContext：
@@ -118,6 +132,280 @@ public class BeanInstanceDemo {
             }
         }
         return null;
+    }
+
+    //DefaultListableBeanFactory:
+    private <T> NamedBeanHolder<T> resolveNamedBean(
+            ResolvableType requiredType, @Nullable Object[] args, boolean nonUniqueAsNull) throws BeansException {
+
+        Assert.notNull(requiredType, "Required type must not be null");
+        //通过类型获取候选的beanName数组
+        String[] candidateNames = getBeanNamesForType(requiredType);
+
+        if (candidateNames.length > 1) {
+            List<String> autowireCandidates = new ArrayList<>(candidateNames.length);
+            for (String beanName : candidateNames) {
+                if (!containsBeanDefinition(beanName) || getBeanDefinition(beanName).isAutowireCandidate()) {
+                    autowireCandidates.add(beanName);
+                }
+            }
+            if (!autowireCandidates.isEmpty()) {
+                candidateNames = StringUtils.toStringArray(autowireCandidates);
+            }
+        }
+
+        if (candidateNames.length == 1) {
+            String beanName = candidateNames[0];
+            return new NamedBeanHolder<>(beanName, (T) getBean(beanName, requiredType.toClass(), args));
+        }else if (candidateNames.length > 1) {
+            Map<String, Object> candidates = new LinkedHashMap<>(candidateNames.length);
+            for (String beanName : candidateNames) {
+                if (containsSingleton(beanName) && args == null) {
+                    Object beanInstance = getBean(beanName);
+                    candidates.put(beanName, (beanInstance instanceof NullBean ? null : beanInstance));
+                }else {
+                    candidates.put(beanName, getType(beanName));
+                }
+            }
+            String candidateName = determinePrimaryCandidate(candidates, requiredType.toClass());
+            if (candidateName == null) {
+                candidateName = determineHighestPriorityCandidate(candidates, requiredType.toClass());
+            }
+            if (candidateName != null) {
+                Object beanInstance = candidates.get(candidateName);
+                if (beanInstance == null || beanInstance instanceof Class) {
+                    beanInstance = getBean(candidateName, requiredType.toClass(), args);
+                }
+                return new NamedBeanHolder<>(candidateName, (T) beanInstance);
+            }
+            if (!nonUniqueAsNull) {
+                throw new NoUniqueBeanDefinitionException(requiredType, candidates.keySet());
+            }
+        }
+        return null;
+    }
+
+    //DefaultListableBeanFactory:
+    public String[] getBeanNamesForType(ResolvableType type) {
+        return getBeanNamesForType(type, true, true);
+    }
+
+    //DefaultListableBeanFactory:
+    public String[] getBeanNamesForType(ResolvableType type, boolean includeNonSingletons, boolean allowEagerInit) {
+        //将此类型解析为{@link java.lang类}，如果类型无法解析，则返回null
+        Class<?> resolved = type.resolve();
+        if (resolved != null && !type.hasGenerics()) {
+            //1>如果可以将type解析成类
+            return getBeanNamesForType(resolved, includeNonSingletons, includeNonSingletons);
+        }else {
+            //2>不能将type解析成类
+            return doGetBeanNamesForType(type, includeNonSingletons, includeNonSingletons);
+        }
+    }
+
+    //DefaultListableBeanFactory:
+    public String[] getBeanNamesForType(@Nullable Class<?> type, boolean includeNonSingletons, boolean allowEagerInit) {
+        if (!isConfigurationFrozen() || type == null || !allowEagerInit) {
+            return doGetBeanNamesForType(ResolvableType.forRawClass(type), includeNonSingletons, allowEagerInit);
+        }
+        Map<Class<?>, String[]> cache =
+                (includeNonSingletons ? this.allBeanNamesByType : this.singletonBeanNamesByType);
+        String[] resolvedBeanNames = cache.get(type);
+        if (resolvedBeanNames != null) {
+            return resolvedBeanNames;
+        }
+        resolvedBeanNames = doGetBeanNamesForType(ResolvableType.forRawClass(type), includeNonSingletons, true);
+        if (ClassUtils.isCacheSafe(type, getBeanClassLoader())) {
+            cache.put(type, resolvedBeanNames);
+        }
+        return resolvedBeanNames;
+    }
+
+    //DefaultListableBeanFactory:
+    private String[] doGetBeanNamesForType(ResolvableType type, boolean includeNonSingletons, boolean allowEagerInit) {
+        List<String> result = new ArrayList<>();
+
+        // Check all bean definitions.
+        for (String beanName : this.beanDefinitionNames) {
+            //只有当bean名称没有定义为其他bean的别名时，才认为bean是合格的
+            if (!isAlias(beanName)) { //不是其他bean的别名
+                try {
+                    RootBeanDefinition mbd = getMergedLocalBeanDefinition(beanName);
+                    // Only check bean definition if it is complete.
+                    if (!mbd.isAbstract() && (allowEagerInit ||
+                            (mbd.hasBeanClass() || !mbd.isLazyInit() || isAllowEagerClassLoading()) &&
+                                    !requiresEagerInitForType(mbd.getFactoryBeanName()))) {
+                        boolean isFactoryBean = isFactoryBean(beanName, mbd);
+                        BeanDefinitionHolder dbd = mbd.getDecoratedDefinition();
+                        boolean matchFound = false;
+                        boolean allowFactoryBeanInit = allowEagerInit || containsSingleton(beanName);
+                        boolean isNonLazyDecorated = dbd != null && !mbd.isLazyInit();
+                        if (!isFactoryBean) {
+                            if (includeNonSingletons || isSingleton(beanName, mbd, dbd)) {
+                                matchFound = isTypeMatch(beanName, type, allowFactoryBeanInit);
+                            }
+                        }
+                        else  {
+                            if (includeNonSingletons || isNonLazyDecorated ||
+                                    (allowFactoryBeanInit && isSingleton(beanName, mbd, dbd))) {
+                                matchFound = isTypeMatch(beanName, type, allowFactoryBeanInit);
+                            }
+                            if (!matchFound) {
+                                // In case of FactoryBean, try to match FactoryBean instance itself next.
+                                beanName = FACTORY_BEAN_PREFIX + beanName;
+                                matchFound = isTypeMatch(beanName, type, allowFactoryBeanInit);
+                            }
+                        }
+                        if (matchFound) {
+                            result.add(beanName);
+                        }
+                    }
+                }catch (CannotLoadBeanClassException | BeanDefinitionStoreException ex) {
+                    if (allowEagerInit) {
+                        throw ex;
+                    }
+                    // Probably a placeholder: let's ignore it for type matching purposes.
+                    LogMessage message = (ex instanceof CannotLoadBeanClassException) ?
+                            LogMessage.format("Ignoring bean class loading failure for bean '%s'", beanName) :
+                            LogMessage.format("Ignoring unresolvable metadata in bean definition '%s'", beanName);
+                    logger.trace(message, ex);
+                    onSuppressedException(ex);
+                }
+            }
+        }
+
+        // Check manually registered singletons too.
+        for (String beanName : this.manualSingletonNames) {
+            try {
+                // In case of FactoryBean, match object created by FactoryBean.
+                if (isFactoryBean(beanName)) {
+                    if ((includeNonSingletons || isSingleton(beanName)) && isTypeMatch(beanName, type)) {
+                        result.add(beanName);
+                        // Match found for this bean: do not match FactoryBean itself anymore.
+                        continue;
+                    }
+                    // In case of FactoryBean, try to match FactoryBean itself next.
+                    beanName = FACTORY_BEAN_PREFIX + beanName;
+                }
+                // Match raw bean instance (might be raw FactoryBean).
+                if (isTypeMatch(beanName, type)) {
+                    result.add(beanName);
+                }
+            }catch (NoSuchBeanDefinitionException ex) {
+                // Shouldn't happen - probably a result of circular reference resolution...
+                logger.trace(LogMessage.format("Failed to check manually registered singleton with name '%s'", beanName), ex);
+            }
+        }
+
+        return StringUtils.toStringArray(result);
+    }
+
+    //SimpleAliasRegistry:
+    public boolean isAlias(String name) {
+        // Map<String, String> aliasMap = new ConcurrentHashMap<>(16)
+        //从别名映射到规范名称
+        return this.aliasMap.containsKey(name);
+    }
+
+    //AbstractBeanFactory:
+    //返回合并的RootBeanDefinition，如果指定的bean对应于子bean定义，则遍历父bean定义
+    protected RootBeanDefinition getMergedLocalBeanDefinition(String beanName) throws BeansException {
+        //首先快速检查并发映射，并使用最少的锁
+        //Map<String, RootBeanDefinition> mergedBeanDefinitions = new ConcurrentHashMap<>(256)
+        RootBeanDefinition mbd = this.mergedBeanDefinitions.get(beanName);
+        if (mbd != null && !mbd.stale) {
+            return mbd;
+        }
+        //如果RootBeanDefinition为空，就重新获取
+        return getMergedBeanDefinition(beanName, getBeanDefinition(beanName));
+    }
+
+    //AbstractBeanFactory:
+    protected RootBeanDefinition getMergedBeanDefinition(String beanName, BeanDefinition bd)
+            throws BeanDefinitionStoreException {
+
+        return getMergedBeanDefinition(beanName, bd, null);
+    }
+
+    //AbstractBeanFactory:
+    protected RootBeanDefinition getMergedBeanDefinition(
+            String beanName, BeanDefinition bd, @Nullable BeanDefinition containingBd)
+            throws BeanDefinitionStoreException {
+
+        synchronized (this.mergedBeanDefinitions) {
+            RootBeanDefinition mbd = null;
+            RootBeanDefinition previous = null;
+
+            // Check with full lock now in order to enforce the same merged instance.
+            if (containingBd == null) {
+                mbd = this.mergedBeanDefinitions.get(beanName);
+            }
+
+            if (mbd == null || mbd.stale) {
+                previous = mbd;
+                mbd = null;
+                if (bd.getParentName() == null) {
+                    // Use copy of given root bean definition.
+                    if (bd instanceof RootBeanDefinition) {
+                        mbd = ((RootBeanDefinition) bd).cloneBeanDefinition();
+                    }
+                    else {
+                        mbd = new RootBeanDefinition(bd);
+                    }
+                }
+                else {
+                    // Child bean definition: needs to be merged with parent.
+                    BeanDefinition pbd;
+                    try {
+                        String parentBeanName = transformedBeanName(bd.getParentName());
+                        if (!beanName.equals(parentBeanName)) {
+                            pbd = getMergedBeanDefinition(parentBeanName);
+                        }
+                        else {
+                            BeanFactory parent = getParentBeanFactory();
+                            if (parent instanceof ConfigurableBeanFactory) {
+                                pbd = ((ConfigurableBeanFactory) parent).getMergedBeanDefinition(parentBeanName);
+                            }
+                            else {
+                                throw new NoSuchBeanDefinitionException(parentBeanName,
+                                        "Parent name '" + parentBeanName + "' is equal to bean name '" + beanName +
+                                                "': cannot be resolved without an AbstractBeanFactory parent");
+                            }
+                        }
+                    }
+                    catch (NoSuchBeanDefinitionException ex) {
+                        throw new BeanDefinitionStoreException(bd.getResourceDescription(), beanName,
+                                "Could not resolve parent bean definition '" + bd.getParentName() + "'", ex);
+                    }
+                    // Deep copy with overridden values.
+                    mbd = new RootBeanDefinition(pbd);
+                    mbd.overrideFrom(bd);
+                }
+
+                // Set default singleton scope, if not configured before.
+                if (!StringUtils.hasLength(mbd.getScope())) {
+                    mbd.setScope(RootBeanDefinition.SCOPE_SINGLETON);
+                }
+
+                // A bean contained in a non-singleton bean cannot be a singleton itself.
+                // Let's correct this on the fly here, since this might be the result of
+                // parent-child merging for the outer bean, in which case the original inner bean
+                // definition will not have inherited the merged outer bean's singleton status.
+                if (containingBd != null && !containingBd.isSingleton() && mbd.isSingleton()) {
+                    mbd.setScope(containingBd.getScope());
+                }
+
+                // Cache the merged bean definition for the time being
+                // (it might still get re-merged later on in order to pick up metadata changes)
+                if (containingBd == null && isCacheBeanMetadata()) {
+                    this.mergedBeanDefinitions.put(beanName, mbd);
+                }
+            }
+            if (previous != null) {
+                copyRelevantMergedBeanDefinitionCaches(previous, mbd);
+            }
+            return mbd;
+        }
     }
 
 }
